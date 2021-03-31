0b89c1f6e304b2ec9be77488152d6382ad15605f
Anders Rune Jensen <arj03@protonmail.ch>
2021-03-31 20:57:55 +0200

    Add db numbers

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Test the performance of a simple server and client.
 
 Streaming 8k messages as json: 1.4s
 Streaming 8k messages as bipf: 1.17s
+
+DB query 8k messages as json: 0.6s
+DB query 8k messages as bipf: 0.5s
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const { author } = require('ssb-db2/operators')
+
 const dir = '/home/arj/.ssb'
 
 //require('rimraf').sync(dir)
@@ -16,6 +18,19 @@ require('ssb-browser-core/core.js').init(dir, {
   }
 })
 
+function query(cb) {
+  SSB.db.getLog().onDrain(() => {
+    console.time("db query")
+    SSB.db.getJITDB().all(author('@6CAxOI3f+LUOVrbAl0IemqiS7ATpQvr9Mdw9LC4+Uv0=.ed25519'), 0, false, false, (err, results) => {
+      console.timeEnd("db query")
+      if (cb) cb()
+    })
+  })
+}
+
 SSB.events.on('SSB: loaded', function() {
+  // twice to warm up cache
+  query(query)
+
   console.log("server is ready net:localhost:8888")
 })
